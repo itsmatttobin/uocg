cda907bdfbdf38303ff097ad0c7be8b6cb0c5c05
Matt Tobin <matt@matttobin.co.uk>
2020-04-04 00:58:08 +0100

    Room setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,44 +5,31 @@ import socketIOClient from 'socket.io-client';
 export default class App extends React.Component<{}, StateType> {
   state: StateType = {
     host: 'http://localhost:4000',
-    message: '',
-    messages: [],
+    roomId: '',
+    room: {},
   }
 
   socket = socketIOClient(this.state.host);
 
-  componentDidMount() {
-    this.socket.on('new message', this.appendMessage);
+  componentDidMount = () => {
+    this.socket.on('update room', (room: any) => {
+      this.setState({ room });
+    });
   }
 
-  handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ message: e.target.value });
+  handleRoomIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ roomId: e.target.value });
   }
 
-  handleClick = () => {
-    this.socket.emit('chat message', this.state.message);
-    this.setState({ message: '' });
-  }
-
-  appendMessage = (message: string) => {
-    this.setState({ messages: [...this.state.messages, message] })
-  }
-
-  renderMessages = () => {
-    return this.state.messages.map(msg => <li key={msg}>{msg}</li>);
+  handleJoinClick = () => {
+    this.socket.emit('join room', this.state.roomId);
   }
 
   render() {
     return (
-      <div className="App">
-        <input type="text" value={this.state.message} onChange={this.handleChange} />
-        <button onClick={this.handleClick}>Send</button>
-
-        <hr/>
-
-        <ul>
-          {this.renderMessages()}
-        </ul>
+      <div>
+        <input type="text" value={this.state.roomId} onChange={this.handleRoomIdChange} />
+        <button onClick={this.handleJoinClick}>Join</button>
       </div>
     );
   }
@@ -50,6 +37,6 @@ export default class App extends React.Component<{}, StateType> {
 
 interface StateType {
   host: string;
-  message: string;
-  messages: string[];
+  roomId: string;
+  room: any;
 }
--- server/app.js
+++ server/app.js
@@ -2,9 +2,30 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const rooms = {
+  abc: ['a', 'b', 'c'],
+  def: ['d', 'e', 'f'],
+}
+
+function updateRoom(id) {
+  io.to(id).emit('update room', rooms[id]);
+}
+
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('new message', msg);
+  let previousId;
+  const safeJoin = currentId => {
+    socket.leave(previousId);
+    socket.join(currentId);
+    previousId = currentId;
+  };
+
+  socket.on('join room', id => {
+    safeJoin(id);
+
+    // Create a new room if one doesn't already exist
+    if (!rooms[id]) {
+      rooms[id] = [];
+    }
   });
 });
 
