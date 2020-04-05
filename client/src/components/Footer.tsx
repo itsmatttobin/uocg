a2097447ee41999d3a57d7027604792c028d8595
Matt Tobin <matt@matttobin.co.uk>
2020-04-05 16:26:52 +0100

    Fixed cors and footer space

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -12,8 +12,8 @@ const Footer = () => (
       <div className="column">
         <div className="content has-text-right">
           <p>
-            UOCG is not affiliated with
-            <a className="has-text-info" href="https://cardsagainsthumanity.com/">Cards Against Humanity.</a><br/>
+            UOCG is not affiliated with <a className="has-text-info" href="https://cardsagainsthumanity.com/">
+              Cards Against Humanity.</a><br/>
             Cards Against Humanity is made available under a Creative Commons BY-NC-SA 2.0 license.<br/>
             "Cards Against Humanity" and the CAH logos are trademarks of Cards Against Humanity LLC.
           </p>
